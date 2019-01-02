3996ff5a424a261f1b076d0bd98d974cafcdb79f
wongprom <jimmybackstrom@hotmail.com>
2019-01-02 13:30:27 +0100

    Add text on hero image.

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,9 @@ const Home = () => {
     <div className="home">
       <div className="home-image-wrapper">
         <img src={ImageEvolution} alt="Human Evolution" />
+        <div className="text-wrapper">
+          <h1>PixaBay API</h1>
+        </div>
       </div>
       <p>Home</p>
     </div>
