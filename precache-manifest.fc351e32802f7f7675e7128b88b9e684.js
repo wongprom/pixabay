eb5ece6a4359cd1fbeb10718baa4c8725079f69e
wongprom <jimmybackstrom@hotmail.com>
2019-01-17 15:06:22 +0100

    Updates

---
+++ precache-manifest.fc351e32802f7f7675e7128b88b9e684.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7a8168d820d34043226c523e9a20f34",
+    "url": "/pixabay/static/media/keps.c7a8168d.jpg"
+  },
+  {
+    "revision": "2958b1144ea331d3eb298c0054f756ca",
+    "url": "/pixabay/static/media/github.2958b114.png"
+  },
+  {
+    "revision": "21079be50574d19df6576665e609943c",
+    "url": "/pixabay/static/media/evolution.21079be5.jpg"
+  },
+  {
+    "revision": "3407f7cbfb516a9f169a",
+    "url": "/pixabay/static/js/runtime~main.3407f7cb.js"
+  },
+  {
+    "revision": "52b42b1e64ac590776eb",
+    "url": "/pixabay/static/js/main.52b42b1e.chunk.js"
+  },
+  {
+    "revision": "338deba8705d1be1ec27",
+    "url": "/pixabay/static/js/1.338deba8.chunk.js"
+  },
+  {
+    "revision": "52b42b1e64ac590776eb",
+    "url": "/pixabay/static/css/main.de083d91.chunk.css"
+  },
+  {
+    "revision": "ddb592a72c8957a4c82fd083e2791b6e",
+    "url": "/pixabay/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/pixabay/precache-manifest.fc351e32802f7f7675e7128b88b9e684.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/pixabay/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
